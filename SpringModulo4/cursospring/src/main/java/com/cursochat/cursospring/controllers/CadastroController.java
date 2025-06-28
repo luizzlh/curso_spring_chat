cccaa17f0b403c706b9c32714cef324c940b538d
Luiz <luizzlh2004@gmail.com>
2025-06-28 18:08:48 -0300

    Inicio do Módulo 4

---
+++ SpringModulo4/cursospring/src/main/java/com/cursochat/cursospring/controllers/CadastroController.java
@@ -0,0 +1,54 @@
+package com.cursochat.cursospring.controllers;
+
+import com.cursochat.cursospring.models.Aluno;
+import com.cursochat.cursospring.repositories.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+
+@Controller
+public class CadastroController {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("aluno", new Aluno());
+        return "cadastro";
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrarAluno(@ModelAttribute Aluno aluno){
+        alunoRepository.save(aluno);
+        return "redirect:/lista";
+    }
+
+    @GetMapping("/lista")
+    public String listarAlunos(Model model){
+        model.addAttribute("alunos", alunoRepository.findAll());
+        return "lista";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editarAluno(@PathVariable int id, Model model) {
+        Aluno aluno = alunoRepository.findById(id).orElseThrow();
+        if(aluno.getId() == id){
+            model.addAttribute("aluno", aluno);
+            return "cadastro";
+        }
+        return "redirect:/lista";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarAluno(@PathVariable int id){
+        alunoRepository.deleteById(id);
+        return "redirect:/lista";
+    }
+}
---
+++ SpringModulo4/cursospring/src/main/java/com/cursochat/cursospring/models/Aluno.java
@@ -0,0 +1,48 @@
+package com.cursochat.cursospring.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String curso;
+
+    public Aluno() {
+    }
+
+    public Aluno(String curso, String nome) {
+        this.curso = curso;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
