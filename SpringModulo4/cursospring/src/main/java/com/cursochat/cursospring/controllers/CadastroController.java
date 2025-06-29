9139a5584dd27afd1ca48fad01cebc2abf6441a1
Luiz <luizzlh2004@gmail.com>
2025-06-29 16:58:11 -0300

    Implementação de filtro de busca, consulta personalizada, validação de formulário e repartição de fragments e layout, além de mudanças estéticas utilizando Bootstrap

--- SpringModulo4/cursospring/src/main/java/com/cursochat/cursospring/controllers/CadastroController.java
+++ SpringModulo4/cursospring/src/main/java/com/cursochat/cursospring/controllers/CadastroController.java
@@ -2,15 +2,15 @@ package com.cursochat.cursospring.controllers;
 
 import com.cursochat.cursospring.models.Aluno;
 import com.cursochat.cursospring.repositories.AlunoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CadastroController {
@@ -18,21 +18,36 @@ public class CadastroController {
     @Autowired
     private AlunoRepository alunoRepository;
 
-    @GetMapping("/")
+    @GetMapping("/cadastro")
     public String index(Model model){
         model.addAttribute("aluno", new Aluno());
         return "cadastro";
     }
 
     @PostMapping("/cadastrar")
-    public String cadastrarAluno(@ModelAttribute Aluno aluno){
+    public String cadastrarAluno(@Valid @ModelAttribute Aluno aluno,
+                                 BindingResult result,
+                                 Model model){
+        if (result.hasErrors()){
+            return "cadastro";
+        }
+
         alunoRepository.save(aluno);
         return "redirect:/lista";
     }
 
     @GetMapping("/lista")
-    public String listarAlunos(Model model){
-        model.addAttribute("alunos", alunoRepository.findAll());
+    public String listarAlunos(@RequestParam(required = false) String nome, Model model){
+        List<Aluno> alunos;
+
+        if (nome != null && !nome.isEmpty()){
+            alunos = alunoRepository.findByNomeContainingIgnoreCase(nome);
+        } else{
+            alunos = alunoRepository.findAll();
+        }
+
+        model.addAttribute("alunos", alunos);
+        model.addAttribute("nome", nome);
         return "lista";
     }
 
--- SpringModulo4/cursospring/src/main/java/com/cursochat/cursospring/models/Aluno.java
+++ SpringModulo4/cursospring/src/main/java/com/cursochat/cursospring/models/Aluno.java
@@ -1,9 +1,7 @@
 package com.cursochat.cursospring.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 @Entity
 public class Aluno {
@@ -11,7 +9,12 @@ public class Aluno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotBlank(message = "O nome é obrigatório!")
+    @Size(min = 2, message = "O nome deve ter no mínimo 2 letras!")
     private String nome;
+
+    @NotBlank(message = "O curso é obrigatório!")
     private String curso;
 
     public Aluno() {
